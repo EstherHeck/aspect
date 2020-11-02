0fcf95554d303b7060bc1724b134e2204e0a6b01
Esther Heckenbach <hecken@gfz-potsdam.de>
2020-11-02 15:27:21 +0100

    radiation damping available for visco_plastic material model only. Compiling fine

--- source/material_model/rheology/drucker_prager.cc
+++ source/material_model/rheology/drucker_prager.cc
@@ -95,7 +95,7 @@ namespace aspect
                                              const double max_yield_stress,
                                              const double radiation_damping_term) const
       {
-        const double yield_stress = compute_yield_stress(cohesion, angle_internal_friction, pressure, max_yield_stressm, radiation_damping_term);
+        const double yield_stress = compute_yield_stress(cohesion, angle_internal_friction, pressure, max_yield_stress, radiation_damping_term);
 
         const double strain_rate_effective_inv = 1./(2.*effective_strain_rate);
 
