26ae56e729d606eeeff65eba5cecf9eda6ff7f17
hecken <hecken@gfz-potsdam.de>
2021-01-27 16:29:19 +0100

    RSF in plasticadditional outputs - needs some more work

--- source/material_model/rheology/visco_plastic.cc
+++ source/material_model/rheology/visco_plastic.cc
@@ -40,6 +40,18 @@ namespace aspect
         names.emplace_back("current_cohesions");
         names.emplace_back("current_friction_angles");
         names.emplace_back("plastic_yielding");
+
+        // if rate and state friction is used make more additional outputs
+// TODO: how can I only create these when RSF is used? I tried:
+//        if (rheology->friction_options.get_use_theta())
+// but then the error occured: rheology was not declared in this scope
+// when I declare it with:
+//       Rheology::FrictionOptions<dim>::declare_parameters (prm);
+// then I get dim was not declared in this scope
+        names.emplace_back("RSF_a");
+        names.emplace_back("RSF_b");
+        names.emplace_back("RSF_L");
+
         return names;
       }
     }
@@ -52,7 +64,10 @@ namespace aspect
       NamedAdditionalMaterialOutputs<dim>(make_plastic_additional_outputs_names()),
       cohesions(n_points, numbers::signaling_nan<double>()),
       friction_angles(n_points, numbers::signaling_nan<double>()),
-      yielding(n_points, numbers::signaling_nan<double>())
+      yielding(n_points, numbers::signaling_nan<double>()),
+      RSF_a(n_points, numbers::signaling_nan<double>()),
+      RSF_b(n_points, numbers::signaling_nan<double>()),
+      RSF_L(n_points, numbers::signaling_nan<double>())
     {}
 
 
@@ -61,7 +76,15 @@ namespace aspect
     std::vector<double>
     PlasticAdditionalOutputs<dim>::get_nth_output(const unsigned int idx) const
     {
-      AssertIndexRange (idx, 3);
+      // TODO: is there a more elegant way to only get those outputs when RSF is used and still maintain the asserts?
+      // TODO: this should have been the following, but that gave me: error: 'else' without a previous 'if'
+      /*
+            if (friction_options.get_use_theta())
+              AssertIndexRange (idx, 6);
+            else
+              AssertIndexRange (idx, 3);
+      */
+      AssertIndexRange (idx, 6);
       switch (idx)
         {
           case 0:
@@ -72,6 +95,30 @@ namespace aspect
 
           case 2:
             return yielding;
+
+          case 3:
+          {
+            if (friction_options.get_use_theta())
+              return RSF_a;
+            else
+              AssertThrow(false, ExcInternalError());
+          }
+
+          case 4:
+          {
+            if (friction_options.get_use_theta())
+              return RSF_b;
+            else
+              AssertThrow(false, ExcInternalError());
+          }
+
+          case 5:
+          {
+            if (friction_options.get_use_theta())
+              return RSF_L;
+            else
+              AssertThrow(false, ExcInternalError());
+          }
 
           default:
             AssertThrow(false, ExcInternalError());
@@ -769,6 +816,21 @@ namespace aspect
                                                                           in.current_cell,
                                                                           phase_function_values).current_friction_angles[j] * weakening_factors[1];
                 plastic_out->friction_angles[i] += 180.0/numbers::PI * volume_fractions[j] * current_friction;
+              }
+
+            // if rate and state friction is used, the additional output fields must be filled
+            if (friction_options.get_use_theta())
+              {
+                plastic_out->RSF_a[i] = 0;
+                plastic_out->RSF_b[i] = 0;
+                plastic_out->RSF_L[i] = 0;
+
+                for (unsigned int j=0; j < volume_fractions.size(); ++j)
+                  {
+                    plastic_out->RSF_a[i] += volume_fractions[j] * friction_options.calculate_depth_dependent_a_and_b(in.position[i], j).first;
+                    plastic_out->RSF_b[i] += volume_fractions[j] * friction_options.calculate_depth_dependent_a_and_b(in.position[i], j).second;
+                    plastic_out->RSF_L[i] += volume_fractions[j] * friction_options.get_critical_slip_distance(in.position[i], j);
+                  }
               }
           }
       }
