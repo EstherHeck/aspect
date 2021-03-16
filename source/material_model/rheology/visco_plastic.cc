a149ac87104237c87b687b9a6b3ef837d48a97bd
EstherHeck <hecken@gfz-potsdam.de>
2021-03-16 18:40:38 +0100

    Lapusta time step does not through error anymore

--- source/material_model/rheology/visco_plastic.cc
+++ source/material_model/rheology/visco_plastic.cc
@@ -530,7 +530,7 @@ namespace aspect
       ViscoPlastic<dim>::
       get_volumetric_composition_mask() const
       {
-        // Store which components to exclude during the volume fraction computation. 
+        // Store which components to exclude during the volume fraction computation.
         ComponentMask composition_mask = strain_rheology.get_strain_composition_mask();
 
         if (use_elasticity)
